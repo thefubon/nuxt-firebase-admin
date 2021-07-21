224e4261c3472b259c4c96730a1f2a8e39253b06
thefubon <43171309+thefubon@users.noreply.github.com>
2021-07-21 22:00:51 +0300

    upd

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="single-post-page container p-4 bg-gray-100">
     <section class="post">
-      <h1 class="post-title text-2xl fotn-bold">Title of the Post</h1>
+      <h1 class="post-title text-2xl fotn-bold">{{ loadedPosts.title }}</h1>
       <div class="post-details">
-        <div class="post-detail">Last update on XXX</div>
-        <div class="post-detail">Written by NAME</div>
+        <div class="post-detail">Last update on {{ loadedPosts.updatedDate }}</div>
+        <div class="post-detail">Written by {{ loadedPosts.author }}</div>
       </div>
-      <p class="post-content">Content of the post</p>
+      <p class="post-content">{{ loadedPosts.content }}</p>
     </section>
     <section class="post-feedback">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, optio dolores commodi neque nisi, eius, deleniti eos velit assumenda natus ea. Explicabo eligendi architecto voluptatibus impedit eveniet harum blanditiis voluptatem.</p>
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loadedPosts: {
+          id: '1',
+          title: "First Post (ID: " + context.route.params.id + ")",
+          previewText: "This my first post!",
+          author: "Anton Fubon",
+          updatedDate: new Date(),
+          content: "Some dummy text which is definitely not the preview text though!",
+          thumbnail: "https://fubon.uk/Maintenance.jpg"
+        }
+      })
+    }, 500)
+  },
+}
+</script>
