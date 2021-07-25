7ef9b45399e033e382884e2a3e0d0d084a10e706
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2021-07-25 13:21:43 +0300

    upd

--- components/TheSlider.vue
+++ components/TheSlider.vue
@@ -3,7 +3,7 @@
     <VueSlickCarousel class="flex" v-bind="slickOptions" ref="carousel">
       <div class="relative" v-for="slide in slides" :key="slide.id">
         <img class="w-full h-full" :src="slide.img" :alt="slide.alt">
-        <div class="absolute left-0 bottom-0 p-6 bg-black bg-opacity-50 text-white w-full">
+        <div class="absolute left-0 bottom-0 p-4 md:p-6 bg-black bg-opacity-50 text-white w-full">
           <h1 class="text-lg md:text-4xl font-bold" v-text="slide.title"></h1>
           <p class="text-sm md:text-base" v-text="slide.description"></p>
         </div>
@@ -13,12 +13,12 @@
       </template>
     </VueSlickCarousel>
     <div class="absolute left-6 inset-y-0 z-10 flex items-center">
-      <button class="relative -top-11" @click="showPrev">
+      <button class="relative -top-8" @click="showPrev">
         <svg class="w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7"></path></svg>
       </button>
     </div>
     <div class="absolute right-6 inset-y-0 z-10 flex items-center">
-      <button class="relative -top-11" @click="showNext">
+      <button class="relative -top-8" @click="showNext">
         <svg class="w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7"></path></svg>
       </button>
     </div>
@@ -34,35 +34,35 @@
             id: 1,
             title: 'Title 1',
             description: 'Description 1',
-            img: 'https://picsum.photos/960/540.jpg', // require('/assets/img/images.jpg'),
+            img: require('/assets/img/slider/slider-1.jpg'),
             alt: 'Alt',
           },
           {
             id: 2,
             title: 'Title 2',
             description: 'Description 2',
-            img: 'https://picsum.photos/960/540.jpg', // require('/assets/img/images.jpg'),
+            img: require('/assets/img/slider/slider-2.jpg'),
             alt: 'Alt 2',
           },
           {
             id: 3,
             title: 'Title 3',
             description: 'Description 3',
-            img: 'https://picsum.photos/960/540.jpg', // require('/assets/img/images.jpg'),
+            img: require('/assets/img/slider/slider-3.jpg'),
             alt: 'Alt 3',
           },
           {
             id: 4,
             title: 'Title 4',
             description: 'Description 4',
-            img: 'https://picsum.photos/960/540.jpg', // require('/assets/img/images.jpg'),
+            img: require('/assets/img/slider/slider-4.jpg'),
             alt: 'Alt 4',
           },
           {
             id: 5,
             title: 'Title 5',
             description: 'Description 5',
-            img: 'https://picsum.photos/960/540.jpg', // require('/assets/img/images.jpg'),
+            img: require('/assets/img/slider/slider-5.jpg'),
             alt: 'Alt 5',
           },
         ],
@@ -93,7 +93,7 @@
 <style>
   /* Dots */
   .slick-dots li {
-    @apply inline-block w-4 h-4 rounded-full bg-white bg-opacity-30 cursor-pointer relative bottom-28;
+    @apply inline-block w-2 h-2 rounded-full bg-white bg-opacity-30 cursor-pointer relative;
   }
   .slick-dots > li.slick-active {
     @apply bg-white;
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="home-page container">
+    <div class="flex sticky top-0 bg-white shadow-md z-10">
+      <scrollactive class="container flex justify-center items-center h-12 text-blue-green-1200 uppercase font-medium text-sm leading-6 space-x-14" active-class="scrollactive" :modifyUrl="false" :offset="45" :duration="800" bezier-easing-value=".5,0,.35,1">
+        <a class="scrollactive-item" href="#scrollId">Scroll menu</a>
+      </scrollactive>
+    </div>
+
     <TheSlider />
+
+    <div class="h-[2000px]"></div>
+    <div id="scrollId">Scroll ID</div>
+    <div class="h-[3000px]"></div>
   </div>
 </template>
+
+<style scoped>
+  .scrollactive {
+    @apply border-b-2 border-indigo-600 mt-0.5;
+  }
+</style>
