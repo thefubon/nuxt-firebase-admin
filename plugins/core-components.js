3a67202f05ec4196152623f559431d7d72669c2d
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2021-08-05 11:26:12 +0300

    upd

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -3,9 +3,7 @@ import Vue from 'vue'
 import AppButton from '~/components/UI/AppButton'
 import AppControlInput from '~/components/UI/AppControlInput'
 import PostList from '~/components/Posts/PostList'
-import VueStripeMenu from '~/components/Navigation/VueStripeMenu'
 
 Vue.component('AppButton', AppButton)
 Vue.component('AppControlInput', AppControlInput)
 Vue.component('PostList', PostList)
-Vue.component('VueStripeMenu', VueStripeMenu)
